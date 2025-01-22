fadc698354cba62a5b31b84699508d878040abe7
JShistory <behappy0916@naver.com>
2025-01-22 13:30:05 +0900

    초기 세팅

---
+++ src/main/java/com/example/dislacfle/DislacfleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dislacfle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DislacfleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DislacfleApplication.class, args);
+    }
+
+}
