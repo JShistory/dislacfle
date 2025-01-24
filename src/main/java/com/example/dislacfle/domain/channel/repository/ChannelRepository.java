0107d7026a90ac4142a5587450bcc2c2a6ae0ed1
JShistory <behappy0916@naver.com>
2025-01-24 15:09:49 +0900

    feat: workSpace, channel repository 추가

---
+++ src/main/java/com/example/dislacfle/domain/channel/repository/ChannelRepository.java
@@ -0,0 +1,7 @@
+package com.example.dislacfle.domain.channel.repository;
+
+import com.example.dislacfle.domain.channel.entity.Channel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChannelRepository extends JpaRepository<Channel, Long> {
+}
---
+++ src/main/java/com/example/dislacfle/domain/workSpace/repository/WorkSpaceRepository.java
@@ -0,0 +1,7 @@
+package com.example.dislacfle.domain.workSpace.repository;
+
+import com.example.dislacfle.domain.workSpace.entity.WorkSpace;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkSpaceRepository extends JpaRepository<WorkSpace, Long> {
+}
