a06adbbe0d8c2ce5d274824c1f8e2b890768529b
JShistory <behappy0916@naver.com>
2025-01-24 16:29:36 +0900

    feat: UserWorkSpaceRepository 생성

---
+++ src/main/java/com/example/dislacfle/domain/userWorkSpace/repository/UserWorkSpaceRepository.java
@@ -0,0 +1,6 @@
+package com.example.dislacfle.domain.userWorkSpace.repository;
+
+import com.example.dislacfle.domain.userWorkSpace.entity.UserWorkSpace;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserWorkSpaceRepository extends JpaRepository<UserWorkSpace, Long> {}
