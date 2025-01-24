5bbb0f57a5b82829959b47ac9488e134de528b38
JShistory <behappy0916@naver.com>
2025-01-24 16:30:33 +0900

    sytle: UserWorkSpace 위치 변경

--- src/main/java/com/example/dislacfle/domain/userWorkSpace/entity/UserWorkSpace.java
+++ src/main/java/com/example/dislacfle/domain/userWorkSpace/entity/UserWorkSpace.java
@@ -1,5 +1,6 @@
-package com.example.dislacfle.domain.user.entity;
+package com.example.dislacfle.domain.userWorkSpace.entity;
 
+import com.example.dislacfle.domain.user.entity.User;
 import com.example.dislacfle.domain.workSpace.entity.WorkSpace;
 import com.example.dislacfle.global.common.BaseEntity;
 import jakarta.persistence.Entity;
