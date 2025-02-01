6fd8ed03e89bf4c27a12072e961f92e2659151fa
JShistory <behappy0916@naver.com>
2025-02-01 20:27:32 +0900

    feat: Channel Entity 생성

--- src/main/java/com/example/dislacfle/domain/channel/entity/Channel.java
+++ src/main/java/com/example/dislacfle/domain/channel/entity/Channel.java
@@ -1,17 +1,28 @@
 package com.example.dislacfle.domain.channel.entity;
 
+import com.example.dislacfle.domain.workSpace.entity.WorkSpace;
 import com.example.dislacfle.global.common.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Channel extends BaseEntity {
     private String channelName;
+    @ManyToOne
+    private WorkSpace workSpace;
+
     @Builder
-    private Channel(String channelName) {
+    private Channel(String channelName, WorkSpace workSpace) {
         this.channelName = channelName;
+        this.workSpace = workSpace;
+    }
+
+    public void setWorkSpace(WorkSpace workSpace) {
+        this.workSpace = workSpace;
     }
 }
