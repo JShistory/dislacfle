4656b399b3fd3ab4321f9c9c1363e960ea2a392b
JShistory <behappy0916@naver.com>
2025-02-02 00:10:18 +0900

    feat: Channel uuid 컬럼 추가

--- src/main/java/com/example/dislacfle/domain/channel/entity/Channel.java
+++ src/main/java/com/example/dislacfle/domain/channel/entity/Channel.java
@@ -2,8 +2,10 @@ package com.example.dislacfle.domain.channel.entity;
 
 import com.example.dislacfle.domain.workSpace.entity.WorkSpace;
 import com.example.dislacfle.global.common.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,17 +14,29 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Channel extends BaseEntity {
+    @Column(name = "uuid", unique = true, nullable = false)
+    private String uuid;
+    @Column(name = "channelName", nullable = false)
     private String channelName;
     @ManyToOne
     private WorkSpace workSpace;
 
     @Builder
-    private Channel(String channelName, WorkSpace workSpace) {
+    private Channel(String uuid, String channelName, WorkSpace workSpace) {
+        this.uuid = uuid;
         this.channelName = channelName;
         this.workSpace = workSpace;
     }
 
     public void setWorkSpace(WorkSpace workSpace) {
         this.workSpace = workSpace;
+    }
+
+    public static Channel createDefaultChannel(WorkSpace workSpace) {
+        return Channel.builder()
+                .uuid(UUID.randomUUID().toString())
+                .channelName("Default Channel")
+                .workSpace(workSpace)
+                .build();
     }
 }
